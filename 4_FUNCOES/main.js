91e39663f3c776b1b17e73b81d6d4aedd327ef23
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-04 10:59:10 -0400

    Update main.js

--- 4_FUNCOES/main.js
+++ 4_FUNCOES/main.js
@@ -1,78 +1,97 @@
-// criando uma função
+// // criando uma função
 
-function minhaFuncao() {
-    console.log("testando");
-}
+// function minhaFuncao() {
+//     console.log("testando");
+// }
 
-minhaFuncao();
+// minhaFuncao();
 
-// funçaõ dentro de uma variavel
-const minhaFuncaoEmVariavel = function() {
-    console.log("função dentro de uma variavel");
-}
+// // funçaõ dentro de uma variavel
+// const minhaFuncaoEmVariavel = function() {
+//     console.log("função dentro de uma variavel");
+// }
 
-minhaFuncaoEmVariavel();
+// minhaFuncaoEmVariavel();
 
-function funcaoComParametro(string) {
-    console.log(`Imprimindo ${string}`);
-}
+// function funcaoComParametro(string) {
+//     console.log(`Imprimindo ${string}`);
+// }
 
-funcaoComParametro('função com parametro');
+// funcaoComParametro('função com parametro');
 
-funcaoComParametro('outro parametro');
+// funcaoComParametro('outro parametro');
 
-// return
+// // return
 
-const a = 10;
-const b = 20;
-const c = 30;
-const d = 40;
+// const a = 10;
+// const b = 20;
+// const c = 30;
+// const d = 40;
 
-function somar(n1, n2) {
-    return n1 + n2;
-}
+// function somar(n1, n2) {
+//     return n1 + n2;
+// }
 
-const resultado = somar(a, b)
+// const resultado = somar(a, b)
 
-console.log(resultado);
+// console.log(resultado);
 
-console.log(somar(c, d));
+// console.log(somar(c, d));
 
-// Arrow Function
-const testeArrow = () => {
-    console.log("arrow function");
-};
+// // Arrow Function
+// const testeArrow = () => {
+//     console.log("arrow function");
+// };
 
-testeArrow();
+// testeArrow();
 
-const parOuImpar = (n) => {
-    if(n % 2 === 0) {
-        console.log("Par");
-        return
-    }
-    console.log("Impar");
-}
+// const parOuImpar = (n) => {
+//     if(n % 2 === 0) {
+//         console.log("Par");
+//         return
+//     }
+//     console.log("Impar");
+// }
 
-parOuImpar(4);
+// parOuImpar(4);
 
-// Mais sobre Arrow Functions
-const raizQuadrada = (x) => {
-    return x * x;
-}
+// // Mais sobre Arrow Functions
+// const raizQuadrada = (x) => {
+//     return x * x;
+// }
 
-console.log(raizQuadrada(5));
+// console.log(raizQuadrada(5));
 
-const raizQuadrada2 = (x) => x*x;
+// const raizQuadrada2 = (x) => x*x;
 
-console.log(raizQuadrada2(5));
+// console.log(raizQuadrada2(5));
 
-const helloWorld = () => console.log("Hello World");
+// const helloWorld = () => console.log("Hello World");
 
-helloWorld();
+// helloWorld();
 
-// Parametros Opcionais
-const multiplication = (m, n) => {
-    if(m === undefined) {
-        
-    }
-}
+// // Parametros Opcionais
+// const multiplication = (m, n) => {
+//     if(n === undefined) {
+//         return m*2;
+//     } else {
+//         return m*n;
+//     };
+// };
+
+// console.log(multiplication(5));
+
+// console.log(multiplication(5, 2));
+
+// const greeting = (name) => {
+//     if(!name) {
+//         console.log('Olá');
+//         return
+//     }
+
+//     console.log(`Olá ${name}`)
+// }
+
+// greeting();
+
+// greeting('Lucas');
