417469d573cf478c7fd4f29cb8583507321f6258
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-10 15:01:09 -0400

    Update main.js

--- 6_ORIENTACAO_A_OBJETOS_JS/main.js
+++ 6_ORIENTACAO_A_OBJETOS_JS/main.js
@@ -37,52 +37,82 @@
 
 // console.log(Object.getPrototypeOf(bool));
 
-// classes basicas (forma antiga de definir classes em JS)
-const cachorro = {
-    raca: null,
-    patas: 4,
+// // classes basicas (forma antiga de definir classes em JS)
+// const cachorro = {
+//     raca: null,
+//     patas: 4,
+// }
+
+// const pastorAlemao = Object.create(cachorro);
+
+// pastorAlemao.raca = "Pastor Alemão";
+
+// console.log(pastorAlemao);
+
+// console.log(pastorAlemao.patas);
+
+// const bullDog = Object.create(cachorro);
+
+// bullDog.raca = "BullDog";
+// console.log(bullDog.raca);
+
+// // função como classe
+// function criarCachorro(nome, raca) {
+//     const cachorro = Object.create({})
+
+//     cachorro.nome = nome;
+//     cachorro.raca = raca;
+
+//     return cachorro;
+// }
+
+// let hulk = criarCachorro("Hulk", "Pinscher");
+// console.log(hulk);
+
+// // funcções como classe
+
+// function Cachorro(nome, raca) {
+//     this.nome = nome;
+//     this.raca = raca;
+// }
+
+// let dog1 = new Cachorro("Hulk", "Pinscher");
+
+// console.log(dog1);
+
+// //metodos na funcção construtora
+// Cachorro.prototype.uivar = () => {
+//     console.log("au au");
+// }
+
+// dog1.uivar();
+
+// // classes es6
+// class CachorroClass {
+//     constructor(nome, raca) {
+//         this.raca = raca;
+//         this.nome = nome;
+//     };
+// };
+
+// const dog2 = new CachorroClass("dog2", "pitbull");
+
+// console.log(dog2);
+
+// console.log(Object.getPrototypeOf(dog2));
+
+// // mais sobre Classes
+class Caminhao {
+    constructor(eixos, cor) {
+        this.eixos = eixos;
+        this.cor = cor;
+    }
+
+    descreverCaminhao() {
+        console.log(`Este Caminhão tem ${this.eixos} eixos, e é da cor ${this.cor}`);
+    }
 }
 
-const pastorAlemao = Object.create(cachorro);
-
-pastorAlemao.raca = "Pastor Alemão";
-
-console.log(pastorAlemao);
-
-console.log(pastorAlemao.patas);
-
-const bullDog = Object.create(cachorro);
-
-bullDog.raca = "BullDog";
-console.log(bullDog.raca);
-
-// função como classe
-function criarCachorro(nome, raca) {
-    const cachorro = Object.create({})
-
-    cachorro.nome = nome;
-    cachorro.raca = raca;
-
-    return cachorro;
-}
-
-let hulk = criarCachorro("Hulk", "Pinscher");
-console.log(hulk);
-
-// funcções como classe
-
-function Cachorro(nome, raca) {
-    this.nome = nome;
-    this.raca = raca;
-}
-
-let dog1 = new Cachorro("Hulk", "Pinscher");
-
-console.log(dog1);
-
-//metodos na funcção construtora
-Cachorro.prototype.uivar = () => {
-    console.log("au au");
-}
-
-dog1.uivar();
+const scania = new Caminhao(6, "Vermelho");
+console.log(scania);
+scania.descreverCaminhao();
