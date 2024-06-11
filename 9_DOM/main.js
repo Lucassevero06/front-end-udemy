b33d4cfab62bb4878024c26d75b18fb1659c2664
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-11 14:33:09 -0400

    upt

---
+++ 9_DOM/main.js
@@ -0,0 +1,29 @@
+// // Movendo-se pelo DOM
+// console.log(document.body);
+
+// console.log(document.body.childNodes);
+
+// console.log(document.body.childNodes[1].textContent);
+
+// // createTextNode
+// const myText = document.createTextNode("Agora vamos colocar mais um titulo");
+// console.log(myText);
+
+// const h3 = document.createElement("h3");
+// h3.appendChild(myText);
+
+// console.log(h3);
+
+// // estilos com Js
+// const mainContainer = document.querySelector("#main-container");
+
+// mainContainer.style.color = "red";
+// mainContainer.style.backgroundColor = "blue";
+// mainContainer.style.paddingBottom = "15px";
+
+// // alterando estilos de varios elementos
+// const listItens = document.getElementsByTagName("li");
+// for(const li of listItens) {
+//     li.style.backgroundColor = "red";
+// }
+
