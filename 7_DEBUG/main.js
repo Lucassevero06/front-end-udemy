c5105c9b2009ebc39d98722df67eea84fa1d0f59
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-11 10:38:37 -0400

    upt

---
+++ 7_DEBUG/main.js
@@ -0,0 +1,55 @@
+// strict
+"use strict";
+
+// opa = "teste";
+
+const opa = "teste";
+
+// const undefined = 10;
+
+// delete [].length;
+
+// console.log
+
+let a = 1;
+let b = 2;
+
+if (a == 1) {
+    a = b + 2;
+}
+
+console.log(a);
+
+for (let i = 0; i < b; i++) {
+    a = a + 2;
+    console.log(a);
+}
+
+if(a > 5) {
+    a = 25;
+}
+console.log(a);
+
+// debugger
+let c = 1;
+let d = 2;
+
+if (c == 1) {
+  c = d + 2;
+}
+
+// debugger;
+
+for (let i = 0; i < 0; i++) {
+    c = c + 2;
+}
+
+console.log("executou o loop");
+
+// debugger;
+
+if(c > 5) {
+    c = 25
+}
+
+//
