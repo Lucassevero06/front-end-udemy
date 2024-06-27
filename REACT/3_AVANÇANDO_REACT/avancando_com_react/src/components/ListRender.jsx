6e3d7ce9d55d5121ffb74a602f0b74bf701f0213
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-27 14:01:49 -0400

    entendendo foreach

--- REACT/3_AVANÇANDO_REACT/avancando_com_react/src/components/ListRender.jsx
+++ REACT/3_AVANÇANDO_REACT/avancando_com_react/src/components/ListRender.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react"
 const ListRender = () => {
 
-    const [list] = useState(["Lucas", "Gabriel", "Severo"])
+    const [list] = useState(["Lucas", "Gabriel", "Severo", "Luan"])
+
+    const [users, setUsers] = useState([
+        {id: 1, name: "Lucas", age: 18},
+        {id: 2, name: "Gabriel", age: 40},
+        {id: 3, name: "Luan", age: 14},
+    ])
 
     return (
         <div>
+            {/* 4 - Render sem key não é a mais recomendada, porém FUNCIONA */}
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            {/* 5 - Render com key é a RECOMENDADA PELA DOCUMENTAÇÃO OFICIAL */}
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age} anos</li>
                 ))}
             </ul>
         </div>
