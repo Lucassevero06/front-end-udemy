ef36c7e9b16ce0373e411ba6ab75c756f2ecd614
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-27 12:56:33 -0400

    funções com renderização

--- REACT/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ REACT/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -5,19 +5,34 @@ const Events = () => {
         console.log("função com evento");
     }
 
-  return (
-    <div>
+    // 8 - Função de renderização
+    const renderSomething = (x) => {
+        if(x) {
+            return <h1>Renderizando isso</h1>
+        } else {
+            return <h1>Renderizando outra coisa</h1>
+
+        }
+    };
+
+    // return 10 > 2 && <p>Carregando....</p>
+
+    return (
         <div>
-            <button onClick={() => {
-                console.log("clicou");
-            }}>Clique aqui</button>
+            <div>
+                <button onClick={() => {
+                    console.log("clicou");
+                }}>Clique aqui</button>
+            </div>
+            {/* 7 - eventos com função */}
+            <div>
+                <button onClick={handleClick}>Clique aqui - com função</button>
+            </div>
+            {/* Função com renderização */}
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
-        {/* 7 - eventos com função */}
-        <div>
-            <button onClick={handleClick}>Clique aqui - com função</button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Events
