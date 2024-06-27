8b9b38721fd3f854c6491a42c7e6e7f190feed1a
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-27 14:43:53 -0400

    if else ternário em react

--- REACT/3_AVANÇANDO_REACT/avancando_com_react/src/components/ConditionalRender.jsx
+++ REACT/3_AVANÇANDO_REACT/avancando_com_react/src/components/ConditionalRender.jsx
@@ -3,11 +3,20 @@ import React from 'react'
 const ConditionalRender = () => {
     const x = 5;
 
+    const name = "João"
+
     return (
         <div>
             {/* 7 - Renderização condicional */}
             <h3>Isso será exibido?</h3>
             {x > 2 && <p>Se x for maior que 2 exibe, sim!</p>}
+            {/* 8 - else  */}
+            <h3>If else ternário em React:</h3>
+            {name === "João" ? (
+                <div>Olá João</div>
+            ) : (
+                <div>Nome não encontrado</div>
+            )}
         </div>
     )
 }
