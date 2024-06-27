11d27a5ea2d15437491cf8b2e3ad2548e7d3eb7c
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-27 13:31:56 -0400

    useStates

--- REACT/3_AVANÇANDO_REACT/avancando_com_react/src/App.jsx
+++ REACT/3_AVANÇANDO_REACT/avancando_com_react/src/App.jsx
@@ -3,16 +3,22 @@ import './App.css'
 // 2 - imagem em assets
 import city from './assets/city.jpg'
 
+// 3 - hook
+import Data from './components/Data'
+
 function App() {
+
   return (
-    <div>
+    <div className="App" style={{paddingBottom: "500px"}}>
       <h1>Avançando em React</h1>
       {/* 1 - imagem em public */}
       <img src="/img1.jpg" alt="imagem" />
       {/* 2 - imagem em assets */}
       <img src={city} alt="outra imagem" />
+      <Data/>
     </div>
   )
+
 }
 
 export default App
