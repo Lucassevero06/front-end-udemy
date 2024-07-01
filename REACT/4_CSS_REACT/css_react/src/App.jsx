a52d142973a7bf92e93439ef3b792e36debf9bd2
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-01 14:17:41 -0400

    css de componente

--- REACT/4_CSS_REACT/css_react/src/App.jsx
+++ REACT/4_CSS_REACT/css_react/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 
+// 2 - css Componente
+import MyComponent from './components/MyComponent'
+
 function App() {
 
   return (
     <div className='App'>
       {/* 1 - css Global */}
       <h1>CSS no React</h1>
+      {/* 2 - css Componente */}
+      <MyComponent/>
+      <p>Pegou o css do componente</p>
     </div>
   )
 }
---
+++ REACT/4_CSS_REACT/css_react/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de Component</h1>
+        <p>Esse paragrafro terá um estilo</p>
+    </div>
+  )
+}
+
+export default MyComponent
