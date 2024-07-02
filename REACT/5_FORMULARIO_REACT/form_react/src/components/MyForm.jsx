6a0965f0a538367ce5bf14a7f266cb1c9d1764be
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-02 07:37:47 -0400

    melhorando conhecimento sobre o useState com manipulação de daddos

--- REACT/5_FORMULARIO_REACT/form_react/src/components/MyForm.jsx
+++ REACT/5_FORMULARIO_REACT/form_react/src/components/MyForm.jsx
@@ -11,7 +11,7 @@ const MyForm = () => {
         setName(e.target.value);
     }
 
-    console.log(name);
+    console.log(name, email);
 
   return (
     <div>
@@ -24,7 +24,7 @@ const MyForm = () => {
             {/* 2-label envolvendo input */}
             <label>
                 <span>E-mail:</span>
-                <input type="text" name="email" id="email" placeholder="Digite o seu E-mail"/>
+                <input type="text" name="email" id="email" placeholder="Digite o seu E-mail" onChange={(e) => setEmail(e.target.value)}/>
             </label>
             <input type="submit" value="Enviar" />
         </form>
