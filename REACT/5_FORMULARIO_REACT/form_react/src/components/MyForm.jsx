42e3b3b05584aa7b588874ec5d16d4c52de5cdf6
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-02 07:29:28 -0400

    label envolvendo o input

--- REACT/5_FORMULARIO_REACT/form_react/src/components/MyForm.jsx
+++ REACT/5_FORMULARIO_REACT/form_react/src/components/MyForm.jsx
@@ -8,8 +8,14 @@ const MyForm = () => {
                 <label htmlFor="name">Nome:</label>
                 <input type="text" id="name" name="name" placeholder="Digite o seu nome"/>
             </div>
+            {/* 2-label envolvendo input */}
+            <label>
+                <span>E-mail:</span>
+                <input type="text" name="email" id="email" placeholder="Digite o seu E-mail"/>
+            </label>
             <input type="submit" value="Enviar" />
         </form>
+        
     </div>
   )
 }
