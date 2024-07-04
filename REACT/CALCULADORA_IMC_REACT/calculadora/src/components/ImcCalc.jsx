d1096efd06ac66e1faed91ffe354ed4cfe0ffec0
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-04 07:55:22 -0400

    criando validação nos inputs

--- REACT/CALCULADORA_IMC_REACT/calculadora/src/components/ImcCalc.jsx
+++ REACT/CALCULADORA_IMC_REACT/calculadora/src/components/ImcCalc.jsx
@@ -13,6 +13,22 @@ const ImcCalc = () => {
         setWeight("");
     }
 
+    const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, "")
+    }
+
+    const handleHeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value)
+
+        setHeight(updatedValue);
+    }
+
+    const handleWeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value)
+
+        setWeight(updatedValue);
+    }
+
     return (
         <div id="calc-container">
             <h2>Calculadora de IMC</h2>
@@ -21,7 +37,7 @@ const ImcCalc = () => {
                     <div className="form-control">
                         <label htmlFor="height">Altura:</label>
                         <input type="text" name="height" id="height" placeholder="exemplo 1,75" 
-                        onChange={(e) => setHeight(e.target.value)}
+                        onChange={(e) => handleHeightChange(e)}
                         value={height}
                         />
                     </div>
@@ -30,7 +46,7 @@ const ImcCalc = () => {
                     <div className="form-control">
                         <label htmlFor="weight">Peso:</label>
                         <input type="text" name="weight" id="weight" placeholder="exemplo 90.5" 
-                        onChange={(e) => setWeight(e.target.value)}
+                        onChange={(e) => handleHeightChange(e)}
                         value={weight}
                         />
                     </div>
