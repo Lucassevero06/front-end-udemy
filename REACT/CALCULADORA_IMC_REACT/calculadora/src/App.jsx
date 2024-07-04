e6cf78c70ff8ec828df9333ad46b6e31142c2f8b
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-04 08:27:18 -0400

    update react

--- REACT/CALCULADORA_IMC_REACT/calculadora/src/App.jsx
+++ REACT/CALCULADORA_IMC_REACT/calculadora/src/App.jsx
@@ -1,11 +1,24 @@
+import { data } from "./data/data";
+import { useState } from 'react'
 import './App.css'
 import ImcCalc from './components/ImcCalc'
+import ImcTable from "./components/ImcTable";
 
 function App() {
 
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+    
+    console.log("Executou");
+  }
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
     return (
       <div className='Container'>
-        <ImcCalc/>
+        {!imc ? <ImcCalc calcImc={calcImc}/> : <ImcTable />}
       </div>
     )
 }
---
+++ REACT/CALCULADORA_IMC_REACT/calculadora/src/components/ImcTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImcCalc from './ImcCalc'
+
+const ImcTable = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ImcTable
