2be67c4b310f3a62e8cdf3e9e6cc125eb4806374
lucas.severo <lucasgabriel31032006@gmail.com>
2024-12-17 08:13:26 -0400

    js

--- REACT/http_react/src/App.jsx
+++ REACT/http_react/src/App.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react";
+import './App.css';
 
 const url = "http://localhost:3000/products";
 
@@ -20,6 +21,28 @@ function App() {
     getData();
   }, [])
 
+  //enviando dados
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  }
+
+
   return (
     <div>
     
@@ -30,7 +53,23 @@ function App() {
           <li key={product.id}>{product.name} - R$ {product.price}</li>
         ))}
       </ul>
-      
+
+      {/* Enviando dados */}
+      <div className="add-product">
+        <form action="" onSubmit={handleSubmit}>
+          <label>
+            <span>Nome</span>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+          </label>
+
+          <label>
+            <span>Preço</span>
+            <input type="text" value={price} onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
+
     </div>
   )
 }
